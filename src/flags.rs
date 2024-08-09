d7511290c4a785c893db2d606d0655c1fb69df8c
hudson <hfinn@autodist.com>
2024-08-09 15:45:15 -0400

    Chip8

--- src/flags.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Debug)]
-pub enum Flags {
-    POS = 1 << 0,
-    ZRO = 1 << 1,
-    NEG = 1 << 2,
-}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-use lc3_vm::vm::VM;
-use std::io;
-
-fn assemble() {
-    todo!("Write assembler");
-}
-
-fn main() -> Result<(), io::Error> {
-    let mut vm = VM::new();
-    let program_path = "./obj/hello.obj";
-    vm.load_to_memory(program_path)?;
-    vm.run();
-    Ok(())
+fn main() {
+    todo!();
 }
