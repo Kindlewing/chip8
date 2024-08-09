21df212fda11f7369cc5feafa2ab4385e4076359
hudson <hfinn@autodist.com>
2024-08-09 18:39:12 -0400

    Chip-8

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    todo!();
+use sdl2::sys::SDL_Point;
+
+enum Register {
+    V0,
+    V1,
+    V2,
+    V3,
+    V4,
+    V5,
+    V6,
+    V7,
+    V8,
+    V9,
+    V10,
+    VA,
+    VB,
+    VC,
+    VD,
+    VE,
+    VF,
 }
+
+struct chip_8 {
+    memory: [u8; 4096],
+    display: [SDL_Point; 64 * 32],
+}
+
+fn main() {}
