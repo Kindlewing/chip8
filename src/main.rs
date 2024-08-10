a037916a0a8e8547cdbd189f2805b6fe31a9c76f
hudson <hfinn@autodist.com>
2024-08-10 12:53:32 -0400

    ibm logo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 use sdl2::sys::SDL_Point;
+use std::time::Duration;
 
 enum Register {
     V0,
@@ -11,7 +15,6 @@ enum Register {
     V7,
     V8,
     V9,
-    V10,
     VA,
     VB,
     VC,
@@ -20,9 +23,63 @@ enum Register {
     VF,
 }
 
-struct chip_8 {
+struct Chip8 {
     memory: [u8; 4096],
     display: [SDL_Point; 64 * 32],
+    pc: u8,
+    i: [u16; 1],
+    stack: Vec<u16>,
+    delay_timer: u8,
+    sound_timer: u8,
+    reg: [u8; 16],
 }
 
-fn main() {}
+impl Chip8 {
+    pub fn new() -> Self {
+        Chip8 {
+            memory: [0; 4096],
+            display: [SDL_Point { x: 0, y: 0 }; 64 * 32],
+            pc: 0,
+            i: [0; 1],
+            stack: Vec::new(),
+            delay_timer: 0,
+            sound_timer: 0,
+            reg: [0; 16],
+        }
+    }
+}
+
+fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let scale: u32 = 10;
+
+    let window = video_subsystem
+        .window("Chip8", 64 * scale, 32 * scale)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'running: loop {
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
